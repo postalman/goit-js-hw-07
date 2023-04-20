e21f2d063b13f04f82b29b021b2aa302678fb005
ChillyRide <sebastianmars78@gmail.com>
2023-04-21 00:56:02 +0300

    Updated

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", onGallery(galleryItems));
+gallery.addEventListener("click", onModal);
+
+let currentInstance = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: "250" });
+
+function onGallery(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+    })
+    .join("");
+}
+
+function onModal (e) {
+    e.preventDefault();
+    currentInstance;
+}
